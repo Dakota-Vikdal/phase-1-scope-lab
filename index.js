72fd69a80928cf161018ed71698b908c164077f2
Dakota Vikdal <dakota.james19@hotmail.com>
2023-02-04 14:48:10 -0500

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'hair';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+
+
+function overwriteBestCustomer() {
+   return bestCustomer = 'maybe bob';
+}
+
+
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'not hair';
+}
